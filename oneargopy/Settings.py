2dccce7e0f2ee990ab52ac1c63d47aa113ac2bc6
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-05-30 15:44:44 -0700

    document for settings class

---
+++ oneargopy/Settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#initialize.py
+#----------------------------------
+# Created By: Savannah Stephenson
+# Creation Date: 05/30/2024
+# Version: 1.0
+#----------------------------------
+""" The Settings class
+    
+"""
+#----------------------------------
+# 
+#
