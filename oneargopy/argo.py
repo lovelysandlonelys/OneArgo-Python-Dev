6a1275b4d7bf3611506284bcffd49c0e7f66f801
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-06-04 13:29:32 -0700

    first comb over

--- oneargopy/argo.py
+++ oneargopy/argo.py
@@ -19,8 +19,7 @@ from Settings import DownloadSettings, SourceSettings
 # System
 from pathlib import Path
 import requests
-import time
-import datetime
+from datetime import datetime
 import shutil
 
 class argo():
@@ -30,7 +29,7 @@ class argo():
     def __init__(self): 
         self.download_settings = DownloadSettings()
         self.source_settings = SourceSettings()
-        self.index_directory = self.download_settings.base_dir.joinpath("Index")
+        self.index_directory = Path(self.download_settings.base_dir.joinpath("Index"))
 
 
     def initialize(self) -> None:
@@ -41,12 +40,9 @@ class argo():
         self.initialize_subdirectories()
 
         # Download files from GDAC to Index directory
-        if self.index_directory.exists(): 
-            for file in self.download_settings.index_files:
-                self.download_index_files(file)
-        else: 
-            print(f'The Index directory does not exist, we need the index directory to save the index files to.')
-
+        for file in self.download_settings.index_files:
+            self.download_index_files(file)
+        
         # Fill in avail_vars variable in the SourceSettings class
         # Fill in dacs variable in the SourceSettings class
 
@@ -84,7 +80,7 @@ class argo():
         if file_path.exists():
 
             # Check if the settings allow  for updates
-            if (self.download_settings.update == 0):
+            if self.download_settings.update == 0:
                 print(f'The download settings have update set to 0, indicating that we do not want to update index files.')
             else: 
                 last_modified_time = Path(file_path).stat().st_mtime
@@ -116,41 +112,33 @@ class argo():
     def try_download(self, file_name: str, update_status: bool):
         """ A function that attempts to download a file from both GDAC sources.
 
-            :param: url : str - The URL to download the file.
+            :param: file_name : str - The name of the file to download
             :param: update_status: bool - True if the file exists and we are trying to update it. False if the file hasn't been downloaded yet. 
         """
         success = False
         iterations = 0
-        while(not success and iterations < self.download_settings.max_attempts):
-            # Try the first host
-            try:
-                with requests.get(self.source_settings.hosts[1], stream=True) as r:
-                    r.raise_for_status()
-                    with open(file_name, 'wb') as f:
-                        shutil.copyfileobj(r.raw, f)
-                success = True
-            except requests.HTTPError as e:
-                print(f'HTTPError encountered: {e}. Trying next host...')
-
-            # Try the second host
-            if (not success):
+        save_path = self.index_directory.joinpath(file_name)
+        while (not success) and (iterations < self.download_settings.max_attempts):
+            # Try both hosts (perfered one is listed first in download settings)
+            for host in self.source_settings.hosts:
+                url = "".join(host, file_name)
                 try:
-                    with requests.get(self.source_settings.hosts[2], stream=True) as r:
+                    with requests.get(url, stream=True) as r:
                         r.raise_for_status()
-                        with open(file_name, 'wb') as f:
+                        with open(save_path, 'wb') as f:
                             shutil.copyfileobj(r.raw, f)
                     success = True
-                except requests.HTTPError as e:
-                    print(f'HTTPError encountered: {e}. Trying both hosts again...')
+                    # Exit the loop if download is successful so we don't try additional
+                    # sources for no reason.
+                    break 
 
-            #Increment Iterations
-            iterations = iterations + 1
+                except requests.RequestException as e:
+                    print(f'Error encountered: {e}. Trying next host...')
+            
+            # Increment Iterations
+            iterations += 1
 
         # If ultimately nothing could be downloaded
-        if (not success): 
-            if (update_status):
-                print(f'WARNING: The file: {file_name} is not the newest version of the file. The newest version could not be downloaded at this time.')
-            else:
-                raise AttributeError(f'Argo could not download {file_name} at this time.')
-        
-
+        if not success: 
+            message = 'Update failed:' if update_status else 'Download failed:'
+            raise Exception(f'{message} {file_name} could not be downloaded.')
