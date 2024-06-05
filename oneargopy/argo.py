af49bbd004dedc8ba83eab7599e1dcb36f7513e8
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-06-05 11:30:36 -0700

    modulizing a bit

--- oneargopy/argo.py
+++ oneargopy/argo.py
@@ -15,19 +15,7 @@
 
 # Local
 from Settings import DownloadSettings, SourceSettings
-
-# System
-from pathlib import Path
-import requests
-from datetime import datetime
-import shutil
-import gzip
-
-# Innitalize Settings
-download_settings = DownloadSettings()
-source_settings = SourceSettings()
-index_directory = Path(download_settings.base_dir.joinpath("Index"))
-
+from setup_functions import initialize_subdirectories, download_index_files
 
 def initialize() -> None:
     """ The initialize function downloads the index files form GDAC and 
@@ -35,15 +23,17 @@ def initialize() -> None:
         DownloadSettings class. 
     """
     print(f'Starting initialize process...')
+    download_settings = DownloadSettings()
+    source_settings = SourceSettings()
 
     # Check for and create subdirectories if needed
     print(f'Checking for subdirectories...')
-    initialize_subdirectories()
+    initialize_subdirectories(download_settings)
 
     # Download files from GDAC to Index directory
     print(f'Downloading index files...')
     for file in download_settings.index_files:
-        download_index_files(file)
+        download_index_files(file, download_settings, source_settings)
     
     # Fill in avail_vars variable in the SourceSettings class
     source_settings.set_avail_vars()
@@ -56,115 +46,4 @@ def initialize() -> None:
         # his any of that still relevant? 
 
     print(f'Initialize is finished!')
-
-def initialize_subdirectories() -> None:
-    """ A function that checks for and creates the necessary folders as 
-        listed in the download settings sub_dir list. 
-    """
-    print(f'Your current download settings are: {download_settings}')
-    for directory in download_settings.sub_dirs:
-        directory_path = download_settings.base_dir.joinpath(directory)
-        if directory_path.exists():
-            print(f'The {directory_path} directory already exists!')
-        else:
-            try:
-                print(f'Creating the {directory} directory!')
-                directory_path.mkdir()
-            except Exception as e:
-                print(f'Failed to create the {directory} directory: {e}')
-
-
-def download_index_files(file_name: str) -> None:
-    """ A function to download and save an index file from GDAC sources. 
-
-        :param: filename : str - The name of the file we are downloading.
-    """
-    # Get the expected filepath for the file
-    file_path = index_directory.joinpath(file_name)
-    gz_file_path = index_directory.joinpath("".join([file_name, ".gz"]))
-
-    # Check if the filepath exists
-    if file_path.exists():
-
-        # Check if the settings allow  for updates
-        if download_settings.update == 0:
-            print(f'The download settings have update set to 0, indicating that we do not want to update index files.')
-        else: 
-            txt_last_modified_time = Path(file_path).stat().st_mtime
-            print(f'The last modified time .txt: {txt_last_modified_time}')
-            gz_last_modified_time = Path(gz_file_path).stat().st_mtime
-            print(f'The last modified time .gx: {gz_last_modified_time}')
-            
-            current_time = datetime.now().timestamp()
-            print(f'The current time: {current_time}')
-            txt_seconds_since_modified = current_time - txt_last_modified_time
-            gz_seconds_since_modified = current_time - gz_last_modified_time
-            print(f'The seconds since modified txt: {txt_seconds_since_modified}')
-            print(f'The seconds since modified gz: {gz_seconds_since_modified}')
-            print(f'The download threshold: {download_settings.update}')
-
-            # Check if the file should be updated
-            if (gz_seconds_since_modified > download_settings.update):
-                print(f'The file: {file_name} needs to be updated.')
-                try_download(file_name ,True)
-            else:
-                print(f'The file: {file_name} does not need to be updated yet.\n')
-
-    # if the file doesn't exist then download it
-    else: 
-        print(f'The file: {file_name} needs to be downloaded.')
-        try_download(file_name, False)
-
-
-def try_download(file_name: str, update_status: bool)-> None:
-    """ A function that attempts to download a file from both GDAC sources.
-
-        :param: file_name : str - The name of the file to download
-        :param: update_status: bool - True if the file exists and we 
-            are trying to update it. False if the file hasn't been 
-            downloaded yet. 
-    """
-    success = False
-    iterations = 0
-    txt_save_path = index_directory.joinpath(file_name)
-    gz_save_path = index_directory.joinpath("".join([file_name, ".gz"]))
-
-    while (not success) and (iterations < download_settings.max_attempts):
-        # Try both hosts (perfered one is listed first in download settings)
-        for host in source_settings.hosts:
-
-            url = "".join([host, file_name, ".gz"])
-
-            print(f'URL we are trying to download from: {url}')
-            print(f'WE are saving {file_name}.gz to {gz_save_path}')
-
-            try:
-                with requests.get(url, stream=True) as r:
-                    r.raise_for_status()
-                    with open(gz_save_path, 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
-
-                print(f'Unzipping {file_name}.gz...')
-                with gzip.open(gz_save_path, 'rb') as gz_file:
-                    with open(txt_save_path, 'wb') as txt_file:
-                        shutil.copyfileobj(gz_file, txt_file)
-                
-                success = True
-                print(f'{file_name}.gz was successfully downloaded and unzipped to {file_name}')
-                
-                # Exit the loop if download is successful so we don't try additional
-                # sources for no reason.
-                break 
-
-            except requests.RequestException as e:
-                print(f'Error encountered: {e}. Trying next host...')
-        
-        # Increment Iterations
-        iterations += 1
-
-    # If ultimately nothing could be downloaded
-    if not success: 
-        message = 'Update failed:' if update_status else 'Download failed:'
-        raise Exception(f'{message} {file_name} could not be downloaded at this time.')
     
