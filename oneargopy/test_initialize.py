26e035b2b10e53402834ff472a147ee8f06da55f
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-07-11 12:10:08 -0700

    commenting out test for phys varaibles being passed only

--- oneargopy/test_initialize.py
+++ oneargopy/test_initialize.py
@@ -46,12 +46,12 @@ print(data)
 data.to_csv('output_five.txt', encoding='utf-8', index=False, na_rep='nan')
 print(f'\n\n')
 
-print(f'Passing DOXY')
-floats = argo.select_profiles(start_date='2024-05-01', end_date='2024-05-02', type='bgc') 
-data = argo.load_float_data(floats, parameters=['TEMP', 'PSAL'])
-print(data)
-data.to_csv('output_five.txt', encoding='utf-8', index=False, na_rep='nan')
-print(f'\n\n')
+# print(f'Passing DOXY')
+# floats = argo.select_profiles(start_date='2024-05-01', end_date='2024-05-02', type='bgc') 
+# data = argo.load_float_data(floats, parameters=['TEMP', 'PSAL'])
+# print(data)
+# data.to_csv('output_five.txt', encoding='utf-8', index=False, na_rep='nan')
+# print(f'\n\n')
 
 # # PROFILE INDEXES TEST
 # argo = Argo()
